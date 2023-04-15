e4d651ea27f7ba2cd773cfedb39a73c348fe6114
Ritesh <riteshrawat937@gmail.com>
2023-04-15 21:06:08 +0530

    "data structures"

---
+++ letftLinkedList.c
@@ -0,0 +1,56 @@
+// linked list implementation to insert node at the left side.
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct LinkedList
+{
+    int data;
+    struct LinkedList *next;
+} sl;
+int main()
+{
+    sl *p = NULL, *left = NULL, *right = NULL;
+    int value, choice;
+    while (1)
+    {
+        p = (sl *)malloc(sizeof(sl));
+        if (p != NULL)
+        {
+            printf("Enter the element to insert :\n");
+            scanf("%d", &value);
+            p->data = value;
+            if (left == NULL && right == NULL)
+            {
+                left = right = p;
+            }
+            else
+            {
+                p->next = left;
+                left = p;
+            }
+            printf("Do you want to continue\n Press 1(Yes) or 0(No) ? :\n");
+            scanf("%d", &choice);
+            if (choice == 0)
+            {
+                break;
+            }
+        }
+        /*
+          right->next = NULL;
+            p = left;
+            while (p != NULL)
+            {
+                printf("Elemet is : %d \n", p->data);      -->This can be done also as this line of code will
+                                                              insert and print element simultaneously.
+                p = p->next;
+            }
+        */
+    }
+    right->next = NULL;
+    p = left;
+    while (p != NULL)
+    {
+        printf("Elemet is : %d \n", p->data);
+        p = p->next;
+    }
+    return (0);
+}
